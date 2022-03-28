e8db175e7ad49bf5cd161e21931b8a4d1114f706
Jacob Martinson <jacobm3@gmail.com>
2022-03-28 12:30:52 -0500

    Add instructions for importing in linux/ubuntu 20 and windows 10

--- ca/00-setup-root-ca.sh
+++ ca/00-setup-root-ca.sh
@@ -21,5 +21,13 @@ vault write ${PKIPATH}/config/urls \
      issuing_certificates="$VAULT_ADDR/v1/pki/ca" \
      crl_distribution_points="$VAULT_ADDR/v1/pki/crl"
 
+# works in ubuntu 20
+# sudo cp ${ROOTNAME}.crt /usr/local/share/ca-certificates
+# sudo update-ca-certificates
 
-
+# WSL2 -> Win10
+# cp ${ROOTNAME}.crt /mnt/c/Users/jacob/Downloads/
+#
+# in admin powershell
+# PS C:\Windows\system32> cd C:\users\jacob\Downloads\
+# PS C:\users\jacob\Downloads> Import-Certificate -FilePath .\hashi-demo-ca-root.2022.04.crt -CertStoreLocation Cert:\LocalMachine\Root
